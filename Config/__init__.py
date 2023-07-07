20e67f421746db4e686b32f534e6115e7b7ee864
Ashna <81171219+anhsaa@users.noreply.github.com>
2023-07-07 09:50:01 +0545

    Add files via upload

---
+++ Config/__init__.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask_restx import Api
+from flask_cors import CORS
+import mysql.connector
+
+
+dbconnection = mysql.connector.connect(host='localhost',
+                                         database='futsaldb',
+                                         user='ashna',
+                                         password='ashna')
+appauthorizations={
+    'apikey':{
+        'type':'apiKey',
+        'in':'header',
+        'name':'Authorization'
+    }
+}
+app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+api = Api(app, version='1.0', title='Futsal',
+    description='Futsal API', authorizations=appauthorizations
+    )
+
+cors = CORS(app)
+
+from book import *
+from user import *   
+from news import * 
+from owner import * 
+from payment import * 
+from venue import*
+from image import*
